7860bd48d10cf6eafb711a8c8ce33f78d69adac7
harsh-accio <rajputharsh9897@gmail.com>
2025-09-18 20:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return regex.test(str);
+	  
 }
-
+ 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
